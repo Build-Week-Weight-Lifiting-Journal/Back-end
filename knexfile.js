4e670d52ddd50a9e946269e73ffb3fbc718b9be7
WilliamBerlin76 <william.berlin76@gmail.com>
2019-11-18 15:05:55 -0600

    removed pool from knexfile, edited two migrations

--- knexfile.js
+++ knexfile.js
@@ -10,11 +10,6 @@ module.exports = {
     seeds: {
       directory: "./database/seeds"
     },
-    pool: {
-      afterCreate: (conn, done) => {
-        conn.run('PRAGMA foreign_keys = ON', done); 
-      },
-    },
   },
 
   testing: {
@@ -47,11 +42,6 @@ module.exports = {
     },
     seeds: {
       directory: "./database/seeds"
-    },
-    pool: {
-      afterCreate: (conn, done) => {
-        conn.run('PRAGMA foreign_keys = ON', done); 
-      },
     },
   },
 };
