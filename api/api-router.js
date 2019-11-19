5ee11f16abe34ff4cffbe6da099a425f6e721c60
Dylan Mestyanek <mestyanekdylan@gmail.com>
2019-11-19 14:14:23 -0600

    Added documentation for all new endpoints

--- api/api-router.js
+++ api/api-router.js
@@ -4,11 +4,11 @@ const restricted = require("../middleware/restricted-middleware");
 const authRouter = require("../auth/auth-router");
 const usersRouter = require("../routes/users-router");
 const workoutRouter = require("../routes/workouts-router");
-const workoutExecerciseRouter = require("../routes/workouts_exercises");
+const workoutExecerciseRouter = require("../routes/workouts_exercises-router");
 
 router.use("/auth", authRouter);
 router.use("/users", /*restricted,*/ usersRouter);
 router.use("/workouts", workoutRouter);
-router.use("/workout-exercises", workoutExecerciseRouter);
+router.use("/workouts", workoutExecerciseRouter);
 
 module.exports = router;
--- models/workouts_exercises-model.js
+++ models/workouts_exercises-model.js
@@ -1,9 +1,52 @@
 const db = require("../config/db-config");
 
 module.exports = {
-    get
+    get,
+    editExercise,
+    remove
 }
 
 function get() {
     return db('workouts_exercises')
+}
+
+async function editExercise(workoutData, workout_id, user_exercise_id) {
+    const { name, reps, sets, region } = workoutData;
+
+    const exercise = await db('exercises')
+            .where({name})
+            .first();
+
+    if(exercise) {
+       return await db('workouts_exercises')
+            .update({
+                reps,
+                sets,
+                workout_id,
+                exercise_id: exercise.id
+            })
+            .where({id: user_exercise_id})
+    } else {
+        const [ id ] = await db('exercises')
+        .insert({ 
+            name,  
+            region
+        });
+
+    return await db('workouts_exercises')
+        .update({
+            reps,
+            sets,
+            workout_id,
+            exercise_id: id
+        })
+        .where({id: user_exercise_id})   
+    }
+};
+
+function remove(id) {
+    return db('workouts_exercises')
+            .where({id})
+            .first()
+            .del()
 }
