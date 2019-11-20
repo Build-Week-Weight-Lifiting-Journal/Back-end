806ed82b8a1423675dd4008855ec64d408a22f0e
Dylan Mestyanek <mestyanekdylan@gmail.com>
2019-11-20 12:41:26 -0600

    users-router.js unit tests complete for each endpoint.

--- tests/users-router.test.js
+++ tests/users-router.test.js
@@ -9,6 +9,7 @@ describe("User Router Tests", () => {
 
     beforeEach(async () => {
         await db('users').truncate();
+        await db('workouts').truncate();
         await db('users').insert({
             username: "testtest",
             email: "test@gmail.com",
@@ -96,12 +97,102 @@ describe("User Router Tests", () => {
             await expect(res.status).toBe(200);
         });
 
+        it("returns status code 400 if user does not have workouts", async () => {
+            const token = generateToken({ id: 1, username: "testtest" });
+            
+            await db('workouts').truncate();
+
+            const res = await request(server)   
+                .get(`/api/users/1/workouts`)
+                .set("authorization", token)
+            
+            await expect(res.status).toBe(400);
+        });
+
         it("returns array of user's workouts", async () => {
             const workouts = await db('workouts').where({ user_id: 1 });
             
             await expect(workouts).toHaveLength(2);
             await expect(workouts[0].name).toBe("Test Workout");            
             await expect(workouts[1].name).toBe("New Workout");
+        });
+    });
+
+    describe("POST /users/:id/workouts", () => {
+        it("returns status code 201", async () => {
+            const token = generateToken({ id: 1, username: "testtest" });
+            const workout = {
+                name: "New Workout",
+                user_id: 1
+            }
+
+            const res = await request(server)   
+                .post(`/api/users/1/workouts`)
+                .send(workout)
+                .set("authorization", token)
+            
+            await expect(res.status).toBe(201);
+        });
+
+        it("returns status code 400 if request body doesn't contain a name for the workout", async () => {
+            const token = generateToken({ id: 1, username: "testtest" });
+            const workout = {
+                user_id: 1
+            }
+
+            const res = await request(server)   
+                .post(`/api/users/1/workouts`)
+                .send(workout)
+                .set("authorization", token)
+            
+            await expect(res.status).toBe(400);
+        });
+
+        it("returns the ID of created workout", async () => {
+            const token = generateToken({ id: 1, username: "testtest" });
+            const workout = {
+                name: "New Workout",
+                user_id: 1
+            }
+            await db('workouts').truncate();
+
+            const res = await request(server)   
+                .post(`/api/users/1/workouts`)
+                .send(workout)
+                .set("authorization", token)
+            
+            await expect(res.body[0]).toBe(1);
+        });
+    });
+
+    describe("GET /users/profile", () => {
+        it("returns status code 200", async () => {
+            const token = generateToken({ id: 1, username: "testtest" });
+
+            const res = await request(server)   
+                .get("/api/users/profile")
+                .set("authorization", token);
+            
+            await expect(res.status).toBe(200);
+        });
+
+        it("returns the user object that is saved to req.body within the restricted middleware", async () => {
+            let token = generateToken({ id: 1, username: "testtest" });
+
+            let res = await request(server)   
+                .get("/api/users/profile")
+                .set("authorization", token);
+                
+            await expect(res.body.username).toBe("testtest");
+            
+            token = generateToken({ id: 2, username: "newuser" });
+                
+            res = await request(server)   
+                .get("/api/users/profile")
+                .set("authorization", token);
+            
+            await expect(res.body.username).toBe("newuser");
+            
         });
     });
 
