94f3a0ec3fa1f3c8706036a445b3d138af845803
Dylan Mestyanek <mestyanekdylan@gmail.com>
2019-11-21 13:55:13 -0600

    added pool object to knexfile production object

--- knexfile.js
+++ knexfile.js
@@ -44,6 +44,10 @@ module.exports = {
     client: "pg",
     connection: process.env.DATABASE_URL,
     useNullAsDefault: true,
+    pool: {
+      min: 2,
+      max: 10
+    },
     migrations: {
       directory: "./database/migrations"
     },
