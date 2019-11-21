9b554705ebbc7cfc86b9c221b5b6c22f84e80cd4
Dylan Mestyanek <mestyanekdylan@gmail.com>
2019-11-21 14:16:31 -0600

    Testing jwt issues when registering

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -17,7 +17,7 @@ router.post("/register", checkIfUsernameIsTaken, (req, res) => {
         user.password = hashedPassword;
 
         Users.add(user)
-        .then(user => res.status(201).json({ user  }))
+        .then(user => res.status(201).json({ user, token }))
         .catch(err => res.status(500).json({ error: "The server encountered an error while registering the users." }));
     } else {
         res.status(400).json({
